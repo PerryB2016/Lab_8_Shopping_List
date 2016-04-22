481a0bb137867389475d9b9a2ed139302e596cde
PerryB2016 <perry-brown@sbcglobal.net>
2016-04-22 13:32:50 -0400

    Lab 8Shopping List

---
+++ Lab_8_v2.js
@@ -0,0 +1,34 @@
+var groceryList = [
+        {
+            name: "strawberries",
+            price: 2.99
+        },
+        {    name: "yogurt",
+            price: 3.49
+        },
+        {    name: "juice",
+            price: 1.99
+        },
+        {    name: "bagels",
+            price: 2.49
+        }
+    ];
+        var total = 0;
+        var list = document.getElementById("list");
+        var foodTotal = document.getElementById("foodTotal");
+        var button = document.getElementById("button");groceryList.forEach(function(item){
+        var newItem = document.createElement("li");
+        newItem.innerText += item.name + ": " + item.price;
+        list.appendChild(newItem);
+        total += item.price;
+        });
+
+        button.addEventListener("click", function(){
+        var userFood = document.getElementById("food").value;
+        var userPrice = document.getElementById("price").value;
+        var newUserFood = document.createElement("li");
+        newUserFood.innerText += userFood + ": " + userPrice;
+        list.appendChild(newUserFood);
+        total += parseFloat(userPrice, 10);
+        foodTotal.innerText = "The total is " + total + ("!");
+        });
